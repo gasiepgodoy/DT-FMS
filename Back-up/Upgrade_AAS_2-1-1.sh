67ddde2b38510975803982e406457b491fc707d0
Vitor Caldana <vitor.caldana@ifsp.edu.br>
2025-07-08 14:14:01 -0300

    Update Upgrade_AAS_2-1-1.sh

--- Back-up/Upgrade_AAS_2-1-1.sh
+++ Back-up/Upgrade_AAS_2-1-1.sh
@@ -1,25 +1,25 @@
 #! /usr/bin/bash
 
-cd /root/ProjetoGemeo/AAS/Distribution
+cd ProjetoGemeo/AAS/Distribution
 curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Distribution.xml
-
-cd /root/ProjetoGemeo/AAS/Handling1
-curl -o nodeset.xmlhttps://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Handling%201.xml
-
-cd /root/ProjetoGemeo/AAS/Handling2
+cd
+cd ProjetoGemeo/AAS/Handling1
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Handling%201.xml
+cd
+cd ProjetoGemeo/AAS/Handling2
 curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Handling%202.xml
-
-cd /root/ProjetoGemeo/AAS/Master
+cd
+cd ProjetoGemeo/AAS/Master
 curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Master.xml
-
-cd /root/ProjetoGemeo/AAS/Processing
+cd
+cd ProjetoGemeo/AAS/Processing
 curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Processing.xml
+cd
+cd ProjetoGemeo/AAS/Robot
 
-cd /root/ProjetoGemeo/AAS/Robot
-
-
-cd /root/ProjetoGemeo/AAS/Sorting
+cd
+cd ProjetoGemeo/AAS/Sorting
 curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Sorting.xml
-
-cd /root/ProjetoGemeo/AAS/Testing
+cd
+cd ProjetoGemeo/AAS/Testing
 curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Testing.xml
