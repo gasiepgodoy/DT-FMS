02bc572a201d39a70127225a58cda2aebf60fc3b
Vitor Caldana <vitor.caldana@ifsp.edu.br>
2025-07-08 14:21:15 -0300

    Update User_Install.sh

--- Back-up/User_Install.sh
+++ Back-up/User_Install.sh
@@ -1,25 +1,25 @@
 #! /usr/bin/bash
 
-cd /root/ProjetoGemeo/AAS/Distribution
+cd ProjetoGemeo/AAS/Distribution
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
-
-cd /root/ProjetoGemeo/AAS/Handling1
+cd
+cd ProjetoGemeo/AAS/Handling1
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
-
-cd /root/ProjetoGemeo/AAS/Handling2
+cd
+cd ProjetoGemeo/AAS/Handling2
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
-
-cd /root/ProjetoGemeo/AAS/Master
+cd
+cd ProjetoGemeo/AAS/Master
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
-
-cd /root/ProjetoGemeo/AAS/Processing
+cd
+cd ProjetoGemeo/AAS/Processing
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
-
-cd /root/ProjetoGemeo/AAS/Robot
+cd
+cd ProjetoGemeo/AAS/Robot
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
-
-cd /root/ProjetoGemeo/AAS/Sorting
+cd
+cd ProjetoGemeo/AAS/Sorting
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
-
-cd /root/ProjetoGemeo/AAS/Testing
+cd
+cd ProjetoGemeo/AAS/Testing
 curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
