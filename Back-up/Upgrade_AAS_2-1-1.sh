3ba23d3aba086146fa61b14ee63b16bb4d753045
Vitor Caldana <vitor.caldana@ifsp.edu.br>
2025-07-08 13:37:17 -0300

    Add files via upload

---
+++ Back-up/Upgrade_AAS_2-1-1.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+cd /root/ProjetoGemeo/AAS/Distribution
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Distribution.xml
+
+cd /root/ProjetoGemeo/AAS/Handling1
+curl -o nodeset.xmlhttps://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Handling%201.xml
+
+cd /root/ProjetoGemeo/AAS/Handling2
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Handling%202.xml
+
+cd /root/ProjetoGemeo/AAS/Master
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Master.xml
+
+cd /root/ProjetoGemeo/AAS/Processing
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Processing.xml
+
+cd /root/ProjetoGemeo/AAS/Robot
+
+
+cd /root/ProjetoGemeo/AAS/Sorting
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Sorting.xml
+
+cd /root/ProjetoGemeo/AAS/Testing
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-06-26%20-%20V2.1.1%20nodeset%20Testing.xml
---
+++ Back-up/User_Install.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+cd /root/ProjetoGemeo/AAS/Distribution
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
+
+cd /root/ProjetoGemeo/AAS/Handling1
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
+
+cd /root/ProjetoGemeo/AAS/Handling2
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
+
+cd /root/ProjetoGemeo/AAS/Master
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
+
+cd /root/ProjetoGemeo/AAS/Processing
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
+
+cd /root/ProjetoGemeo/AAS/Robot
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
+
+cd /root/ProjetoGemeo/AAS/Sorting
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
+
+cd /root/ProjetoGemeo/AAS/Testing
+curl -o users.json https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/users.json
