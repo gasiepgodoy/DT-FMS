2423b55c062cb23edc1aa350a9298fd9ebfdec43
Vitor Caldana <vitor.caldana@ifsp.edu.br>
2025-12-11 09:47:25 -0300

    Update Node-RED

---
+++ Back-up/Upgrade_AAS_2-4.sh
@@ -0,0 +1,25 @@
+#! usr/bin/bash
+
+cd ProjetoGemeo/AAS/Distribution
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Distribution.xml
+cd
+cd ProjetoGemeo/AAS/Handling1
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Handling%201.xml
+cd
+cd ProjetoGemeo/AAS/Handling2
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Handling%202.xml
+cd
+cd ProjetoGemeo/AAS/Master
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Master.xml
+cd
+cd ProjetoGemeo/AAS/Processing
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Processing.xml
+cd
+cd ProjetoGemeo/AAS/Robot
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Robot%20%2B%20Storage.xml
+cd
+cd ProjetoGemeo/AAS/Sorting
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Sorting.xml
+cd
+cd ProjetoGemeo/AAS/Testing
+curl -o nodeset.xml https://raw.githubusercontent.com/gasiepgodoy/DT-FMS/refs/heads/Node-Red-Software/2025-11-19%20-%20V2.4%20nodeset%20Testing.xml
