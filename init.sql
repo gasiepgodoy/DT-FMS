b8bf2f510eabd10ba57c41964bb4d88461feebf2
tomoriolu <augustin35renaud@gmail.com>
2026-04-02 11:36:29 +0200

    add file to setup environment

---
+++ init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Products (
+    product_type CHAR(1) PRIMARY KEY,
+    Station1Stop BOOLEAN DEFAULT FALSE,
+    Station1Op VARCHAR(50),
+    Station2Stop BOOLEAN DEFAULT FALSE,
+    Station2Op VARCHAR(50),
+    Station3Stop BOOLEAN DEFAULT FALSE,
+    Station3Op VARCHAR(50),
+    Station4Stop BOOLEAN DEFAULT FALSE,
+    Station4Op VARCHAR(50),
+    Station5Stop BOOLEAN DEFAULT FALSE,
+    Station5Op VARCHAR(50),
+    Station6Stop BOOLEAN DEFAULT FALSE,
+    Station6Op VARCHAR(50)
+);
+
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    nb_product INT NOT NULL,
+    product_type CHAR(1),
+    FOREIGN KEY (product_type) REFERENCES Products(product_type)
+);
+
+CREATE TABLE Production (
+    productSN INT PRIMARY KEY,
+    order_id INT,
+    cart_id INT,
+    current_station ENUM('Station1', 'Station2', 'Station3', 'Station4', 'Station5', 'Station6'),
+    visited_stations VARCHAR(255),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
