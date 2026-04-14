5d83693dae89747f27adbb4143a4ac07fe03bb6a
tomoriolu <augustin35renaud@gmail.com>
2026-04-14 11:04:28 +0200

    Add MATLAB interface for each stations

---
+++ MATLAB/Station1ACK.m
@@ -0,0 +1,18 @@
+brokerAddress = "tcp://127.0.0.1";
+port = 1884; 
+
+response = "ACK";
+station_number = 1;
+
+mqClient = mqttclient(brokerAddress, Port=port);
+fprintf("Successfully connected to broker\n")
+
+topic  = "test";
+        
+payloadReady = struct( ...
+    "station",  station_number, ...
+    "response", response ...
+);
+        
+write(mqClient, topic, jsonencode(payloadReady));
+fprintf("Message sent\n");
